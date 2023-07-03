3b4831caf999628297671e4d0cff37c557b664ed
Sergey Voznyuk <sn.voznyuk@gmail.com>
2023-07-03 13:26:15 +0300

    Reformat GRuleHeader component

--- src/components/GRule/GRuleHeader.vue
+++ src/components/GRule/GRuleHeader.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 const emit = defineEmits(['add-rule'])
 
 function addRule() {
@@ -12,22 +11,12 @@ function addRule() {
     <div class="container flex justify-between align-center py-4 mx-auto">
       <h1 class="h1">Add/Edit Rules</h1>
       <div class="btn-block flex gap-2">
-        <button
-            class="btn light"
-            @click.prevent="addRule"
-        >
-          Add New Rule
-        </button>
-        <button class="btn light">
-          Save Rule Group
-        </button>
+        <button class="btn light" @click.prevent="addRule">Add New Rule</button>
+        <button class="btn light">Save Rule Group</button>
         <button class="btn light">Back</button>
       </div>
     </div>
   </header>
 </template>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
