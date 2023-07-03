f32871fa099a36966878cfc561d3607313eac23d
Sergey Voznyuk <sn.voznyuk@gmail.com>
2023-07-03 13:23:08 +0300

    Update rules list

--- src/components/GRule/GRuleList.vue
+++ src/components/GRule/GRuleList.vue
@@ -2,7 +2,7 @@
 const props = defineProps({
   rules: {
     type: Object,
-    default:() => {}
+    default: () => {}
   },
   activeIndex: {
     type: Number,
@@ -17,30 +17,25 @@ function setActive(index) {
 function deleteRule(index) {
   emit('deleteRule', index)
 }
-
 </script>
 
 <template>
   <div>
     <div
-        v-for="(rule, index) in props.rules"
-        :key="rule.id"
-        class="rule-item"
-        :class="{
-          'bg-blue-400 text-white border-transparent': index === activeIndex,
-          'bg-blue-50 cursor-pointer': index !== activeIndex,
-        }"
-        @click="setActive(index)"
+      v-for="(rule, index) in props.rules"
+      :key="rule.id"
+      class="rule-item"
+      :class="{
+        'bg-blue-400 text-white border-transparent': index === activeIndex,
+        'bg-blue-50 cursor-pointer': index !== activeIndex
+      }"
+      @click="setActive(index)"
     >
       <div class="text-2xl">{{ rule.name }}</div>
       <div>{{ rule.desc }}</div>
-      <button class="absolute right-4 top-8" @click.stop="deleteRule(index)">
-        x
-      </button>
+      <button class="absolute right-4 top-8" @click.stop="deleteRule(index)">x</button>
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
