b6dc4e9f7b10f56c38a9f9ddbc0bc5348b383f42
Sergey Voznyuk <sn.voznyuk@gmail.com>
2023-07-03 13:27:06 +0300

    Add GruleOperator && GruleOperand components

---
+++ src/components/GRule/GRuleOperand.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  operand: {
+    type: Object,
+    required: true
+  },
+  index: {
+    type: Number,
+    required: true
+  }
+})
+const emit = defineEmits(['update-operand'])
+
+function updateValue(e) {
+  emit('update-operand', {
+    index: props.index,
+    obj: { [key]: e.target.value }
+  })
+}
+
+const key = Object.keys(props.operand)[0]
+</script>
+
+<template>
+  <div class="flex items-center px-4">
+    <input
+      :value="operand[key]"
+      @input="updateValue"
+      class="font-bold p-2 text-center rounded-md bg-transparent border-b border-gray-500 focus:bg-white"
+    />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/GRule/GRuleOperator.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { operations } from '@/helpers/constants'
+import { computed, ref } from 'vue'
+
+defineOptions({
+  operations
+})
+
+const emit = defineEmits(['updateOperator', 'addExpression'])
+
+const props = defineProps({
+  operator: {
+    type: String,
+    required: true
+  },
+  index: {
+    type: Number,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  }
+})
+
+const showVariants = ref(false)
+
+const groupOperator = computed(
+  () => Object.keys(operations.groupLogic).filter((key) => key !== props.operator)[0]
+)
+
+const variants = computed(() => {
+  if (props.index > 0) {
+    return operations.groupLogic[groupOperator.value]
+  }
+  return operations[props.type]
+})
+
+function changeOperator(operator, index, grouping) {
+  if (props.operator) {
+    emit('updateOperator', { operator, index, grouping })
+  } else {
+    emit('addExpression', operator)
+  }
+  showVariants.value = false
+}
+</script>
+
+<template>
+  <div class="cursor-pointer my-2 self-start">
+    <span
+      class="text-center py-2 px-4 rounded-md inline-block relative shadow-md whitespace-nowrap"
+      :class="{
+        'bg-blue-400  w-48': props.type === 'compare',
+        'w-24': props.type !== 'compare',
+        'bg-green-300 ml-12': props.operator === 'and',
+        'bg-yellow-200 ml-12': props.operator === 'or',
+        'border-2': props.operator === ''
+      }"
+      @click="showVariants = !showVariants"
+      >{{ operations[type][operator]?.str || 'Add' }}</span
+    >
+    <div
+      v-show="showVariants"
+      class="variants bg-white border rounded-md absolute w-auto text-center z-10"
+    >
+      <template v-if="index > 0">
+        <div
+          v-for="value in variants"
+          :key="value.toString()"
+          class="p-2 whitespace-nowrap"
+          @click="changeOperator(groupOperator, index, value.grouping)"
+        >
+          {{ value.str }}
+        </div>
+      </template>
+      <template v-else>
+        <div
+          v-for="(value, key) in variants"
+          :key="key"
+          class="p-2 whitespace-nowrap"
+          @click="changeOperator(key, index, null)"
+        >
+          {{ value.str }}
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
