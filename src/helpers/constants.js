a00522b1b6787bd31d8940482acd2c894e5593c2
Sergey Voznyuk <sn.voznyuk@gmail.com>
2023-07-03 13:22:25 +0300

    Add constants

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,51 +1,61 @@
 export const operations = {
   logic: {
     and: {
-      sign: "&&",
-      str: "AND",
+      sign: '&&',
+      str: 'AND'
     },
     or: {
-      sign: "||",
-      str: "OR",
-    },
+      sign: '||',
+      str: 'OR'
+    }
+  },
+  groupLogic: {
+    or: [
+      { grouping: true, str: '<OR>' },
+      { grouping: true, str: '>OR<' }
+    ],
+    and: [
+      { grouping: true, str: '<AND>' },
+      { grouping: false, str: '>AND<' }
+    ]
   },
   compare: {
     eq: {
-      sign: "=",
-      str: "is equal to",
+      sign: '=',
+      str: 'is equal to'
     },
     not: {
-      sign: "!=",
-      str: "is not equal to",
+      sign: '!=',
+      str: 'is not equal to'
     },
     qt: {
-      sign: ">",
-      str: "is more then",
+      sign: '>',
+      str: 'is more then'
     },
     gte: {
-      sign: ">=",
-      str: "is more or equal to",
+      sign: '>=',
+      str: 'is more or equal to'
     },
     lt: {
-      sign: "<",
-      str: "is less then",
+      sign: '<',
+      str: 'is less then'
     },
     lte: {
-      sign: "<=",
-      str: "is less or equal to",
-    },
+      sign: '<=',
+      str: 'is less or equal to'
+    }
   },
   math: {
-    bor: "|",
-    band: "&",
-    plus: "+",
-    minus: "-",
-    div: "/",
-    mul: "*",
-    mod: "%",
-  },
-};
+    bor: '|',
+    band: '&',
+    plus: '+',
+    minus: '-',
+    div: '/',
+    mul: '*',
+    mod: '%'
+  }
+}
 
-export const actions = ["set", "call", "obj", "const"];
+export const actions = ['set', 'call', 'obj', 'const']
 
-export const types = ["obj", "string", "integer", "float", "bool"];
+export const types = ['obj', 'string', 'integer', 'float', 'bool']
