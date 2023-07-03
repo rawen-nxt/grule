51fa4a265a15e936f1ad2e14cef445e27608abec
Sergey Voznyuk <sn.voznyuk@gmail.com>
2023-07-03 13:25:45 +0300

    Add GruleLogic component

--- src/components/GRule/GRuleEditor.vue
+++ src/components/GRule/GRuleEditor.vue
@@ -1,10 +1,11 @@
 <script setup>
-import useRulesStore from '@/stores/Rules';
-import {storeToRefs} from 'pinia';
+import useRulesStore from '@/stores/Rules'
+import { storeToRefs } from 'pinia'
+import GRuleActions from '@/components/GRule/GRuleActions.vue'
+import GRuleLogic from '@/components/GRule/GRuleLogic.vue'
 
 const rulesStore = useRulesStore()
-const {rule} = storeToRefs(rulesStore)
-
+const { rule } = storeToRefs(rulesStore)
 </script>
 
 <template>
@@ -12,36 +13,25 @@ const {rule} = storeToRefs(rulesStore)
     <div class="row flex">
       <div class="form-block flex flex-col">
         <label>Rule Name</label>
-        <input
-            type="text"
-            class=""
-            name="name"
-            v-model="rule.name"
-        />
+        <input type="text" class="" name="name" v-model="rule.name" />
       </div>
       <div class="form-block flex flex-col">
         <label>Description</label>
-        <input
-            type="text"
-            name="desc"
-            v-model="rule.desc"/>
+        <input type="text" name="desc" v-model="rule.desc" />
       </div>
       <div class="form-block flex flex-col">
         <label>Silence</label>
-        <input
-            type="number"
-            name="silence"
-            v-model="rule.silence"
-        />
+        <input type="number" name="silence" v-model="rule.silence" />
       </div>
     </div>
     <div class="row">
       <div class="title">Conditions</div>
-
+      <GRuleLogic :expression-id="rule.rootId" :isRoot="true" :key="rule.rootId" />
     </div>
-
+    <div class="row">
+      <GRuleActions />
+    </div>
   </div>
 </template>
 
 <style scoped></style>
-
---
+++ src/components/GRule/GRuleLogic.vue
@@ -0,0 +1,59 @@
+<script setup>
+import GRuleCompare from '@/components/GRule/GRuleCompare.vue'
+import GRuleOperator from '@/components/GRule/GRuleOperator.vue'
+import useRulesStore from '@/stores/Rules'
+
+const props = defineProps({
+  isRoot: {
+    type: Boolean,
+    default: false
+  },
+  expressionId: {
+    type: String,
+    required: true
+  }
+})
+
+const rulesStore = useRulesStore()
+const expression = rulesStore.getExpressionById(props.expressionId)
+
+function addExpression(operator) {
+  rulesStore.addExpression(operator)
+}
+
+function updateOperator({ operator, index, grouping }) {
+  rulesStore.updateOperator(props.expressionId, operator, index, grouping)
+}
+</script>
+
+<template>
+  <template v-if="['and', 'or'].includes(expression.operator)">
+    <div
+      class="parent flex p-4 flex-col items-center items-stretch rounded-md relative"
+      :class="{
+        'bg-green-100 shadow-md': expression.operator === 'and',
+        'bg-yellow-100 shadow-md': expression.operator === 'or'
+      }"
+    >
+      <template v-for="(operand, index) in expression.operands" :key="operand">
+        <GRuleOperator
+          v-if="index > 0"
+          :index="expression.operands.length > 2 ? index : 0"
+          :type="'logic'"
+          :operator="expression.operator"
+          @update-operator="updateOperator($event)"
+        />
+
+        <GRuleLogic :expression-id="operand" />
+      </template>
+    </div>
+  </template>
+  <template v-else>
+    <GRuleCompare :expression-id="props.expressionId" :hideClose="false" />
+  </template>
+  <div v-if="isRoot" class="flex justify-center">
+    <GRuleOperator :type="'logic'" @add-expression="addExpression" operator="" />
+  </div>
+</template>
+
+<style scoped></style>
